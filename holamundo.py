eb7954e4b2eb92cc244b555b3d23b0d0429541da
Matias Piccone <picconematias@gmail.com>
2025-07-17 16:28:11 -0300

    git commit -m "primer cambio"

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,16 @@
-print('Hola mundo111')
+# Variables y tipos de datos
+nombre = "Matías"
+edad = 25
+altura = 1.81
+es_estudiante = True
+
+# Condicional simple
+if edad >= 18:
+    print("Sos mayor de edad")
+
+# Función simple
+def saludar(nombre):
+    print("Hola", nombre)
+
+saludar("Python")
+
