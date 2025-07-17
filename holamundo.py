56cddfa903aea65a41e6a5c69dffb8b6536cce39
Matias Piccone <picconematias@gmail.com>
2025-07-17 17:10:50 -0300

    primer cambio 2

--- holamundo.py
+++ holamundo.py
@@ -1,16 +1,7 @@
-# Variables y tipos de datos
-nombre = "Matías"
-edad = 25
-altura = 1.81
-es_estudiante = True
+# Este es un comentario en Python
 
-# Condicional simple
-if edad >= 18:
-    print("Sos mayor de edad")
-
-# Función simple
-def saludar(nombre):
-    print("Hola", nombre)
-
-saludar("Python")
-
+"""
+Este es un comentario
+de varias 
+lineas
+"""
---
+++ proyecto1.py
@@ -0,0 +1,19 @@
+# Variables y tipos de datos
+nombre = "Matías"
+edad = 15
+altura = 1.81
+es_estudiante = True
+
+# Condicional simple
+if edad >= 18:
+    print("Sos mayor de edad")
+else:
+    print("Sos menor de edad")
+
+# Función simple
+def saludar(nombre):
+    print("Hola", nombre)
+
+saludar("Python")
+
+print("hi there")
