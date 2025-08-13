e72406058cf565ea0595f16dfae7937e7e45e1d1
Matias Piccone <picconematias@gmail.com>
2025-08-12 21:16:35 -0300

    1

---
+++ exercise_operadors.py
@@ -0,0 +1,9 @@
+edad = 25
+altura = 1.81
+complejo = 1 + 2j
+
+base = input("Base:")
+altura = input("Altura:")
+area_triangulo = (float(base) * float(altura)) / 2
+print("Área del triángulo:", area_triangulo)
+
